ed67ce82c33f258cf84e65006e58f35a8cbab50b
GregoriSimei <gregorisimei@gmail.com>
2022-10-22 14:55:56 -0300

    Delete Cylinder

--- src/application/use-cases/cyliders/DeleteCylinder.ts
+++ src/application/use-cases/cyliders/DeleteCylinder.ts
@@ -1,7 +1,7 @@
-import { IBranchRepository } from 'src/application/repositories/IBranchRepository';
 import { ICompanyRepository } from 'src/application/repositories/ICompanyRepository';
 import { ICylinderRepository } from 'src/application/repositories/ICylinderRepository';
 import { inject, injectable } from 'tsyringe';
+import { ValidationError } from 'yup';
 import { IDeleteCylinder } from './IDeleteCylinder';
 
 @injectable()
@@ -9,17 +9,36 @@ export class DeleteCylinderUseCase implements IDeleteCylinder {
   constructor(
     @inject('ICompanyRepository')
     private companyRepoitory: ICompanyRepository,
-    @inject('ICompanyRepository')
-    private branchRepoitory: IBranchRepository,
-    @inject('ICompanyRepository')
+    @inject('ICylinderRepository')
     private cylinderRepoitory: ICylinderRepository,
   ) {}
 
-  execute(
+  async execute(
     companyId: string,
     branchId: string,
-    cilynderId: string,
+    cylinderId: string,
   ): Promise<boolean> {
-    throw new Error('Method not implemented.');
+    if (!companyId || !branchId || !cylinderId) {
+      throw new ValidationError('Invalid Parameter');
+    }
+
+    const companyFound = await this.companyRepoitory.findById(companyId);
+    if (!companyFound) {
+      throw new ValidationError('Company not exist');
+    }
+
+    const branchFound = companyFound.branches?.find((branch) => branch.id === branchId);
+    if (!branchFound) {
+      throw new ValidationError('Branch not exist');
+    }
+
+    const cylinderFound = branchFound.cylinders?.some((cylinder) => cylinder.id === cylinderId);
+    if (!cylinderFound) {
+      throw new ValidationError('Cylinder not exist');
+    }
+
+    await this.cylinderRepoitory.delete(cylinderId);
+
+    return true;
   }
 }
