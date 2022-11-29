ec1325349c483d149d6e462e0abd64fab41c0240
GregoriSimei <gregorisimei@gmail.com>
2022-11-29 02:35:34 -0300

    Created typeOrm alert model

--- src/infra/repository/postgress/models/Company.ts
+++ src/infra/repository/postgress/models/Company.ts
@@ -1,9 +1,11 @@
+import { IAlert } from 'src/application/dtos/IAlert';
 import { IBranch } from 'src/application/dtos/IBranch';
 import { ICompany } from 'src/application/dtos/ICompany';
 import { IEmployee } from 'src/application/dtos/IEmployee';
 import {
   Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn
 } from 'typeorm';
+import { Alert } from './Alert';
 import { Branch } from './Branch';
 import { Employee } from './Employee';
 
@@ -23,6 +25,9 @@ export class Company implements ICompany {
 
     @OneToMany(() => Branch, (branch) => branch.company)
       branches?: IBranch[];
+
+    @OneToMany(() => Alert, (alert) => alert.company)
+      alerts?: IAlert[];
 
     @UpdateDateColumn()
       updated_at?: Date;
